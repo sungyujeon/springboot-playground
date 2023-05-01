844054089a72953ea9d5b96cc2fb2fad415504c0
sungyujeon <sungyujeon91@gmail.com>
2023-05-01 16:56:32 +0900

    [#1] refactor: main method 내에서 일괄 처리하도록 변경

--- src/main/java/playground/springboot/CustomSpringbootApplication.java
+++ src/main/java/playground/springboot/CustomSpringbootApplication.java
@@ -1,13 +1,11 @@
 package playground.springboot;
 
-import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -22,38 +20,30 @@ public class CustomSpringbootApplication {
     public static void main(String[] args) {
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 
-        WebServer webServer = serverFactory.getWebServer(new CustomServletContextInitializer());
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("frontcontroller", new HttpServlet() {
+                HelloController helloController = new HelloController();
+
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    // 인증, 보안, 다국어, 공통 기능 등
+                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {  // binding
+                        String name = req.getParameter("name");
+
+                        String ret = helloController.hello(name);  // HelloController에 처리를 위임(mapping)
+
+                        resp.setStatus(HttpStatus.OK.value());
+                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        resp.getWriter().println(ret);
+                    } else if (req.getRequestURI().equals("/user")){
+                        // user request...
+                    } else {
+                        resp.setStatus(HttpStatus.NOT_FOUND.value());
+                    }
+                }
+            })
+            .addMapping("/*");
+        });
         webServer.start();
-    }
-
-    private static class CustomServletContextInitializer implements ServletContextInitializer {
-        @Override
-        public void onStartup(ServletContext servletContext) {
-            servletContext.addServlet("frontcontroller", new CustomHttpServlet())
-                          .addMapping("/*");
-        }
-    }
-
-    private static class CustomHttpServlet extends HttpServlet {
-
-        private final HelloController helloController = new HelloController();
-
-        @Override
-        protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-            // 인증, 보안, 다국어, 공통 기능 등
-            if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {  // binding
-                String name = req.getParameter("name");
-
-                String ret = helloController.hello(name);  // HelloController에 처리를 위임(mapping)
-
-                resp.setStatus(HttpStatus.OK.value());
-                resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                resp.getWriter().println(ret);
-            } else if (req.getRequestURI().equals("/user")){
-                // user request...
-            } else {
-                resp.setStatus(HttpStatus.NOT_FOUND.value());
-            }
-        }
     }
 }
