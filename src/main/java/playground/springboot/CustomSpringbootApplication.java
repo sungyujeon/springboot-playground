00fe3bdd1fa3bb6407e551dc5162ac8768de35ec
sungyujeon <sungyujeon91@gmail.com>
2023-05-01 14:09:20 +0900

    [#1] feat: servlet context 내 servlet 등록

--- src/main/java/playground/springboot/CustomSpringbootApplication.java
+++ src/main/java/playground/springboot/CustomSpringbootApplication.java
@@ -1,15 +1,41 @@
 package playground.springboot;
 
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpStatus;
+
+import java.io.IOException;
 
 public class CustomSpringbootApplication {
 
     public static void main(String[] args) {
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 
-        WebServer webServer = serverFactory.getWebServer();
+        WebServer webServer = serverFactory.getWebServer(new CustomServletContextInitializer());
         webServer.start();
+    }
+
+    private static class CustomServletContextInitializer implements ServletContextInitializer {
+        @Override
+        public void onStartup(ServletContext servletContext) {
+            servletContext.addServlet("hello", new CustomHttpServlet())
+                          .addMapping("/hello");
+        }
+    }
+
+    private static class CustomHttpServlet extends HttpServlet {
+        @Override
+        protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            resp.setStatus(HttpStatus.OK.value());
+            resp.setHeader("Content-Type", "text/plain");
+            resp.getWriter().println("Hello Servlet");
+        }
     }
 }
