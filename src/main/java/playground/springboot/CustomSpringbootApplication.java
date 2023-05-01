d4487006285dbe4d3714dfa11310e03678c0b9d1
sungyujeon <sungyujeon91@gmail.com>
2023-05-01 16:22:07 +0900

    [#1] feat: uri 세분화, http method 구분

--- src/main/java/playground/springboot/CustomSpringbootApplication.java
+++ src/main/java/playground/springboot/CustomSpringbootApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -27,19 +28,26 @@ public class CustomSpringbootApplication {
     private static class CustomServletContextInitializer implements ServletContextInitializer {
         @Override
         public void onStartup(ServletContext servletContext) {
-            servletContext.addServlet("hello", new CustomHttpServlet())
-                          .addMapping("/hello");
+            servletContext.addServlet("frontcontroller", new CustomHttpServlet())
+                          .addMapping("/*");
         }
     }
 
     private static class CustomHttpServlet extends HttpServlet {
         @Override
         protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-            String name = req.getParameter("name");
+            // 인증, 보안, 다국어, 공통 기능 등
+            if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+                String name = req.getParameter("name");
 
-            resp.setStatus(HttpStatus.OK.value());
-            resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-            resp.getWriter().println("Hello " + name);
+                resp.setStatus(HttpStatus.OK.value());
+                resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                resp.getWriter().println("Hello " + name);
+            } else if (req.getRequestURI().equals("/user")){
+                // user request...
+            } else {
+                resp.setStatus(HttpStatus.NOT_FOUND.value());
+            }
         }
     }
 }
